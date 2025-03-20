c6b5c6c4b77a6051001f7604fca12254b20bd27f
Jihyun <qlfus010@naver.com>
2025-03-21 02:18:25 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -3,18 +3,15 @@ board=[[0]*100 for _ in range(100)] #100x100 배열을 0으로 초기화
 
 for i in range(num): #색종이 수만큼 반복
     x,y=map(int,input().split())
-    x-=1
-    y-=1
-    for j in range(10):
-        for k in range(10):
-            if(board[x+j][y+k]!=1): #색종이의 넓이만큼 배열값을 1로 변경(이미 덮어져있다면 변경x)
-                board[x+j][y+k]=1
+
+    for i in range(x,x+10): #(변경) x1~x1+10까지 순회하는 코드 range로 작성
+        for j in range(y,y+10): #(변경) y1~y1+10까지 순회하는 코드 range로 작성
+            board[i][j]=1 #색종이로 뒤덮여 있는 지역은 1로 변경
+
 
 area = 0
 for i in range(100): #100x100 배열을 순회하며 색종이로 덮여있는 (배열값이 1인) 넓이를 구함
-    for j in range(100):
-        if(board[i][j]==1):
-            area+=1
-
+    area+=board[i].count(1) #(변경)2차원 리스트이므로, board[i]에서 1인 값을 추출(count(1))하여 누적합을 구한다.
 print(area)
      
+# 참고 : https://ji-gwang.tistory.com/325

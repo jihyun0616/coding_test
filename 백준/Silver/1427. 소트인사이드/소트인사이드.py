7698fe388433aac9b186cb5d897f3edba6d1a690
Jihyun <qlfus010@naver.com>
2025-04-08 23:04:07 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,6 +1,2 @@
-N=input()
-number_list=[]
-for i in str(N):
-    number_list.append(int(i))
-
-print("".join(map(str,sorted(number_list,reverse=True))))
+number_list=list(input()) 
+print("".join(sorted(number_list,reverse=True)))

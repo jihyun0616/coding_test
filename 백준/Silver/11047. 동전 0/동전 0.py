e14a236ea6db221a0dd7a0f753fa6a3bb3317a71
Jihyun <qlfus010@naver.com>
2025-05-20 23:45:31 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,33 @@
 N, K = map(int, input().split())
 
-money_list = [int(input()) for _ in range(N)]
+money_list = []
+for i in range(N):
+    money = int(input())
+    money_list.append(money)
+
+
+money_list.sort()
+
+flag = 0
+for i in range(len(money_list)):
+    if K < money_list[i]:
+        flag = money_list[i - 1]
+        break
+    elif K == money_list[i]:
+        flag = money_list[i]
+        break
+    else:
+        flag = money_list[-1] 
+        
 money_list.sort(reverse=True)
 
-count=0
-for i in money_list:
-    if i<=K:
-        count+=K//i #몫
-        K=K%i #나머지
+count = 0
+for coin in money_list:
+    if coin > flag:
+        continue
+    if K >= coin:
+        num = K // coin
+        count += num
+        K -= coin * num
 
 print(count)
-
-
